fcdb8819be55ebbba7b6b0abb08b7a5a886bd6ce
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-03-31 23:26:41 +0200

    feat(ch02): add example trials display to the home page

---
+++ src/BlazingTrails/BlazingTrails.Client/Features/Home/ViewModels/Trail.cs
@@ -0,0 +1,14 @@
+namespace BlazingTrails.Client.Features.Home.ViewModels;
+
+public class Trail
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public int TimeInMinutes { get; set; }
+    public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
+    public int LengthKm { get; set; }
+    public IEnumerable<RouteInstruction> Route { get; set; }
+}
