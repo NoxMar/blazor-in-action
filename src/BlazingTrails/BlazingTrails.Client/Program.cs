00f3ca4277235f26a09449a6d04dc2d063c19ada
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-04-05 08:32:09 +0200

    feat(ap02): dispatch request from home page to api trough MediatR

--- src/BlazingTrails/BlazingTrails.Client/Program.cs
+++ src/BlazingTrails/BlazingTrails.Client/Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazingTrails.Client;
-using BlazingTrails.Client.Domain;
-using BlazingTrails.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<ITrailsService, TrailService>();
 
 await builder.Build().RunAsync();
