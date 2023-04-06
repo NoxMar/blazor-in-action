79e6d1d22551f3a44c3a54e30caa085cf0105e7d
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-04-06 20:44:58 +0200

    chore(ch05): wire fluent validation

--- src/BlazingTrails/BlazingTrails.Api/Program.cs
+++ src/BlazingTrails/BlazingTrails.Api/Program.cs
@@ -1,4 +1,6 @@
 using BlazingTrails.Api.Persistence;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<BlazingTrails.Shared.Contracts.GetTrailsRequest>();
 
 var app = builder.Build();
 
