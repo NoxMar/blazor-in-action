f30a48a1b4b7d3bce043c29e1d54edd6a33f1254
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-04-12 19:01:14 +0200

    feat(ch08): enable making RouteMap read-only

--- src/BlazingTrails/BlazingTrails.ComponentLibrary/Map/RouteMap.razor.js
+++ src/BlazingTrails/BlazingTrails.ComponentLibrary/Map/RouteMap.razor.js
@@ -1,52 +1,50 @@
-export function initialize(hostElement, routeMapComponent, existingEndpoints) 
-{
+export function initialize(hostElement, routeMapComponent, existingEndpoints, isReadOnly) {
+    console.log(`isReadOnly?: ${isReadOnly}`);
     hostElement.map = L.map(hostElement)
         .setView([51.700, -0.10], 3);
- 
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 18,
         opacity: .75
     }).addTo(hostElement.map);
- 
+
     hostElement.waypoints = [];
     hostElement.lines = [];
-    
-    if (existingEndpoints && existingEndpoints.length > 0)
-    {
-        existingEndpoints.forEach(cord =>
-        {
+
+    if (existingEndpoints && existingEndpoints.length > 0) {
+        existingEndpoints.forEach(cord => {
             let waypoint = L.marker(cord);
             waypoint.addTo(hostElement.map);
             hostElement.waypoints.push(waypoint);
             let line = L.polyline(hostElement.waypoints
-                .map(m => m.getLatLng()),
-                { color: 'var(--brand)'})
+                    .map(m => m.getLatLng()),
+                {color: 'var(--brand)'})
                 .addTo(hostElement.map);
             hostElement.lines.push(line);
         })
     }
-    
-    if (hostElement.waypoints.length > 0)
-    {
+
+    if (hostElement.waypoints.length > 0) {
         var waypoints = new L.featureGroup(hostElement.waypoints);
         hostElement.map.fitBounds(waypoints
             .getBounds().pad(1));
     }
-    
-    hostElement.map.on('click', function (e) {
-        let waypoint = L.marker(e.latlng);
-        waypoint.addTo(hostElement.map);
-        hostElement.waypoints.push(waypoint);
-        let line = L.polyline(hostElement.waypoints
-            .map(m => m.getLatLng()), { color: 'var(--brand)' })
-            .addTo(hostElement.map);
-        hostElement.lines.push(line);
-        
-        routeMapComponent.invokeMethodAsync('WaypointAdded', e.latlng.lat, e.latlng.lng);
-    });
-}
 
+    if (!isReadOnly) {
+        hostElement.map.on('click', function (e) {
+            let waypoint = L.marker(e.latlng);
+            waypoint.addTo(hostElement.map);
+            hostElement.waypoints.push(waypoint);
+            let line = L.polyline(hostElement.waypoints
+                .map(m => m.getLatLng()), {color: 'var(--brand)'})
+                .addTo(hostElement.map);
+            hostElement.lines.push(line);
+
+            routeMapComponent.invokeMethodAsync('WaypointAdded', e.latlng.lat, e.latlng.lng);
+        });
+    }
+}
 export function deleteLastWaypoint(hostElement)
 {
     if (hostElement.waypoints.length > 0)
