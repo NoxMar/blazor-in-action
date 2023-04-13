9418e4082e3b6a56b14dfccf0c04bf95fd7bdd46
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-04-13 20:32:23 +0200

    feat(ch09): connect Auth0 with the frontend

--- src/BlazingTrails/BlazingTrails.Client/Program.cs
+++ src/BlazingTrails/BlazingTrails.Client/Program.cs
@@ -9,4 +9,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddOidcAuthentication(cfg =>
+{
+    builder.Configuration.Bind("Auth0", cfg.ProviderOptions);
+    cfg.ProviderOptions.ResponseType = "code";
+});
+
 await builder.Build().RunAsync();
