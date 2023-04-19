ce4d4d79e0b4f0d46ad8ab06b43adad5e48f9968
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-04-19 22:01:22 +0200

    feat: use `Blazor.Bootstrap` instead of regular blazor

--- src/BlazingTrails/BlazingTrails.Client/Program.cs
+++ src/BlazingTrails/BlazingTrails.Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazingTrails.Client;
 using BlazingTrails.Client.AppState;
+using BlazorBootstrap;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
@@ -9,6 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazorBootstrap();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<AppState>();
 
