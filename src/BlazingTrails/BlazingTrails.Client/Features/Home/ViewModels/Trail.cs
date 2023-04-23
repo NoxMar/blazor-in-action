88809a18b5a6dbd6efec5a053cca145ddff3ce76
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-04-23 14:45:28 +0200

    feat: show delete button only when user is owner of the trail

--- src/BlazingTrails/BlazingTrails.Client/Features/Home/ViewModels/Trail.cs
+++ src/BlazingTrails/BlazingTrails.Client/Features/Home/ViewModels/Trail.cs
@@ -12,6 +12,7 @@ public class Trail
     public int TimeInMinutes { get; set; }
     public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
     public int LengthKm { get; set; }
+    public string Owner = string.Empty;
     public IEnumerable<RouteInstruction> Route { get; set; }
     public List<LatLong> Waypoints { get; set; } = new();
 }
